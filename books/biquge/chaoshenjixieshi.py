128bcafcba32e57cc261fe529a284d7f6bb4effc
foreveruand <43624948+foreveruand@users.noreply.github.com>
2019-09-27 14:31:40 +0800

    Add files via upload

---
+++ books/biquge/chaoshenjixieshi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return chaoshenjixieshi
+
+
+class chaoshenjixieshi(xxbiqugebase):
+    title = u'[小说]超神机械师'
+    description = u''
+    feeds = 'https://www.biquge5200.cc/76_76215/'
---
+++ books/biquge/diyixulie.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return diyixulie
+
+
+class diyixulie(xxbiqugebase):
+    title = u'[小说]第一序列'
+    description = u'会说话的肘子'
+    feeds = 'https://www.biquge5200.cc/117_117581/'
